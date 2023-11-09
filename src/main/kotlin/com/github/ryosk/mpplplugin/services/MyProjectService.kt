7c07894a98131f6d992e4c828079d7d9051f8c9b
GitHub Action <action@github.com>
2023-11-09 00:57:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/ryosk/mpplplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ryosk/mpplplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ryosk.mpplplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ryosk.mpplplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/ryosk/mpplplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ryosk/mpplplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ryosk.mpplplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ryosk.mpplplugin.MyBundle
+import com.github.ryosk.mpplplugin.services.MyProjectService
 import javax.swing.JButton
 
 
