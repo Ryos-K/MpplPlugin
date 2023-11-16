4ef6afefe06fb7566f76a3cf3375b056cf3f10ae
ry05k2ulv <ryosuke.10117@gmail.com>
2023-11-16 18:31:17 +0900

    feat: Add Code Completion

--- src/main/kotlin/com/github/ryosk/mpplplugin/language/MpplKeywordCompletionContributor.kt
+++ src/main/kotlin/com/github/ryosk/mpplplugin/language/MpplKeywordCompletionContributor.kt
@@ -1,16 +1,140 @@
 package com.github.ryosk.mpplplugin.language
 
+import com.github.ryosk.mpplplugin.language.psi.MpplTokenSets
 import com.github.ryosk.mpplplugin.language.psi.MpplTypes
 import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.Lookup
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.codeInsight.template.impl.TemplateSettings
+import com.intellij.formatting.Indent
+import com.intellij.openapi.editor.EditorModificationUtil
 import com.intellij.psi.PsiElement
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PlatformPatterns.*
 import com.intellij.util.ProcessingContext
 
 class MpplKeywordCompletionContributor : CompletionContributor() {
+    private val insideMpplPattern = psiElement(MpplTypes.NAME).withLanguage(MpplLanguage.INSTANCE)
 
     init {
+        extend(
+            CompletionType.BASIC,
+            insideMpplPattern,
+            MpplKeywordCompletionProvider(
+                AddSpaceInsertHandler.INSTANCE,
+                "and",
+                "div",
+                "do",
+                "else",
+                "not",
+                "of",
+                "or",
+                "then",
+            )
+        )
+        extend(
+            CompletionType.BASIC, insideMpplPattern, MpplKeywordCompletionProvider(
+                null,
+                "boolean",
+                "char",
+                "end",
+                "false",
+                "integer",
+                "true",
+            )
+
+        )
+        extend(
+            CompletionType.BASIC, insideMpplPattern, MpplKeywordCompletionProvider(
+                programLookUpElement(),
+                varLookUpElement(),
+                procedureLookUpElement(),
+                beginEndLookUpElement(),
+                ifThenLookUpElement(),
+                ifThenElseLookUpElement(),
+                whileLookUpElement(),
+                arrayLookUpElement(),
+                embeddedFunctionLookUpElement("read"),
+                embeddedFunctionLookUpElement("readln"),
+                embeddedFunctionLookUpElement("write"),
+                embeddedFunctionLookUpElement("writeln"),
+            )
+        )
 
     }
+
+    private fun programLookUpElement() = LookupElementBuilder
+        .create("program")
+        .withTailText(" ...")
+        .withInsertHandler { context, item ->
+            context.document.insertString(context.selectionEndOffset, " ;\nbegin\n end")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun varLookUpElement() = LookupElementBuilder
+        .create("var")
+        .withTailText(" ...")
+        .withInsertHandler { context, item ->
+            context.document.insertString(context.selectionEndOffset, " : ;")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+
+    private fun procedureLookUpElement() = LookupElementBuilder
+        .create("procedure")
+        .withTailText(" ...")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, " ;")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun beginEndLookUpElement() = LookupElementBuilder
+        .create("begin")
+        .withTailText(" ...")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, "  end")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun ifThenLookUpElement() = LookupElementBuilder
+        .create("if")
+        .withTailText(" ... then")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, "  then")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun ifThenElseLookUpElement() = LookupElementBuilder
+        .create("if")
+        .withTailText(" ... then ... else")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, "  then  else")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun whileLookUpElement() = LookupElementBuilder
+        .create("while")
+        .withTailText(" ... do")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, "  do")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun arrayLookUpElement() = LookupElementBuilder
+        .create("array")
+        .withTailText(" ...")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, " [] of ")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
+    private fun embeddedFunctionLookUpElement(func: String) = LookupElementBuilder
+        .create(func)
+        .withTailText(" ...")
+        .withInsertHandler { context, _ ->
+            context.document.insertString(context.selectionEndOffset, "();")
+            EditorModificationUtil.moveCaretRelatively(context.editor, 1)
+        }
+
 }
+
---
+++ src/main/kotlin/com/github/ryosk/mpplplugin/language/MpplKeywordCompletionProvider.kt
@@ -0,0 +1,34 @@
+package com.github.ryosk.mpplplugin.language
+
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.util.ProcessingContext
+import com.intellij.util.containers.toArray
+
+class MpplKeywordCompletionProvider(
+    private vararg val keywordLookUpElements: LookupElementBuilder,
+) : CompletionProvider<CompletionParameters>() {
+
+    constructor(
+        insertHandler: InsertHandler<LookupElement>?,
+        vararg keywords: String,
+    ) : this(
+        *keywords.map {
+            LookupElementBuilder.create(it).withInsertHandler(insertHandler)
+        }.toTypedArray()
+    )
+
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        keywordLookUpElements.forEach {
+            result.addElement(it)
+        }
+    }
+}
